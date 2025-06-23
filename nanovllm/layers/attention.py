fix attn calculation

nanovllm/layers/attention.py
@@ -57,18 +57,36 @@ class Attention(nn.Module):
 
     def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, attention_mask: torch.Tensor | None = None):
         if attention_mask is not None:
-            # Attention with no caching
-            q = q * self.scale
-
-            o: torch.Tensor
+            # print(positions.shape) # torch.Size([492])
+            # print(hidden_states.shape) # torch.Size([492, 1024])
+            # print(q.shape, k.shape, v.shape) # torch.Size([511, 2048]) torch.Size([511, 1024]) torch.Size([511, 1024])
+            
             q = q.view(-1, self.num_heads, self.head_dim)
-            k = k.view(-1, self.num_kv_heads, self.head_dim)
+            k = k.view(-1, self.num_kv_heads, self.head_dim) 
             v = v.view(-1, self.num_kv_heads, self.head_dim)
 
-            attn = torch.matmul(q, k.transpose(-2, -1))
-            attn = attn + attention_mask
-            attn = torch.softmax(attn, dim=-1)
-            o = torch.matmul(attn, v)
+            num_key_value_groups = self.num_heads // self.num_kv_heads
+            k = k.unsqueeze(2).expand(-1, -1, num_key_value_groups, -1).reshape(-1, self.num_heads, self.head_dim)
+            v = v.unsqueeze(2).expand(-1, -1, num_key_value_groups, -1).reshape(-1, self.num_heads, self.head_dim)
+
+            attn_weights = torch.matmul(q, k.transpose(-2, -1)) * self.scale
+
+            # print(attention_mask.shape) # torch.Size([511])
+            # print(attn_weights.shape) # torch.Size([511, 16, 16])
+            # print(k.shape) # torch.Size([511, 16, 128])
+            # print(q.shape) # torch.Size([511, 16, 128])
+            # print(v.shape) # torch.Size([511, 16, 128])
+            # print(self.num_heads) # 16
+            # print(self.head_dim) # 128
+
+            # attn_weights = attn_weights.masked_fill((attention_mask.unsqueeze(1).unsqueeze(1) == 0), float("-inf"))
+            attn_weights = attn_weights.masked_fill(attention_mask[None, None, :] == 0, float("-inf"))
+
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(q.dtype)
+            attn_weights = nn.functional.dropout(attn_weights, p=0.0, training=self.training)
+
+            o = torch.matmul(attn_weights, v)
+            o = o.contiguous()
         else:
             o: torch.Tensor
             q = q.view(-1, self.num_heads, self.head_dim)

nanovllm/layers/layernorm.py
@@ -2,48 +2,36 @@ import torch
 from torch import nn
 
 
-class RMSNorm(nn.Module):
+# TODO: I removed in-place operations from RMS norm (went from 490 -> 440 TPS)
 
-    def __init__(
-        self,
-        hidden_size: int,
-        eps: float = 1e-6,
-    ) -> None:
+class RMSNorm(nn.Module):
+    def __init__(self, hidden_size: int, eps: float = 1e-6) -> None:
         super().__init__()
         self.hidden_size = hidden_size
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(hidden_size))
 
-    @torch.compile
-    def rms_forward(
-        self,
-        x: torch.Tensor,
-    ) -> torch.Tensor:
+    def rms_forward(self, x: torch.Tensor) -> torch.Tensor:
         orig_dtype = x.dtype
-        x = x.to(torch.float32)
-        var = x.pow(2).mean(dim=-1, keepdim=True)
-        x.mul_(torch.rsqrt(var + self.eps))
-        x = x.to(orig_dtype).mul_(self.weight)
-        return x
+        x_f32 = x.to(torch.float32)
+        var = x_f32.pow(2).mean(dim=-1, keepdim=True)
+        x_norm = x_f32 * torch.rsqrt(var + self.eps)
+        x_norm = x_norm.to(orig_dtype) * self.weight
+        return x_norm
 
-    @torch.compile
     def add_rms_forward(
-        self,
-        x: torch.Tensor,
-        residual: torch.Tensor,
-    ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
+        self, x: torch.Tensor, residual: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         orig_dtype = x.dtype
-        x = x.to(torch.float32).add_(residual.to(torch.float32))
-        residual = x.to(orig_dtype)
-        var = x.pow(2).mean(dim=-1, keepdim=True)
-        x.mul_(torch.rsqrt(var + self.eps))
-        x = x.to(orig_dtype).mul_(self.weight)
-        return x, residual
+        x_f32 = x.to(torch.float32) + residual.to(torch.float32)
+        residual_out = x_f32.to(orig_dtype)
+        var = x_f32.pow(2).mean(dim=-1, keepdim=True)
+        x_norm = x_f32 * torch.rsqrt(var + self.eps)
+        x_norm = x_norm.to(orig_dtype) * self.weight
+        return x_norm, residual_out
 
     def forward(
-        self,
-        x: torch.Tensor,
-        residual: torch.Tensor | None = None,
+        self, x: torch.Tensor, residual: torch.Tensor | None = None
     ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
         if residual is None:
             return self.rms_forward(x)