2fe5096784454386c27ee0d4bd57d0d7ce03a310
David Heineman <david@davidheineman.com>
2025-06-27 21:30:08 -0700

    dist_port

--- nanovllm/config.py
+++ nanovllm/config.py
@@ -16,6 +16,7 @@ class Config:
     eos: int = -1
     kvcache_block_size: int = 256
     num_kvcache_blocks: int = -1
+    dist_port: int = 29522
 
     def __post_init__(self):
         assert os.path.isdir(self.model)
